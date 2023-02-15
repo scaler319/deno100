15be4aec8e6738df4d9a91b535054b0590b0665b
tangshh <1320528371@qq.com>
2023-02-15 21:14:55 +0800

    21：13第一次提交

---
+++ src/main/java/com/example/demo100/Demo100Application.java
@@ -0,0 +1,13 @@
+package com.example.demo100;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo100Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo100Application.class, args);
+    }
+
+}
